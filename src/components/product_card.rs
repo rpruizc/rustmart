52876b008ee6c555b8d302af0b89c251720c5728
Rodolfo Ruiz <rpruiz@me.com>
2020-09-25 13:52:09 -0500

    Products, cards and cart button

---
+++ src/components/product_card.rs
@@ -0,0 +1,42 @@
+use crate::types::Product;
+use yew::prelude::*;
+
+pub struct ProductCard {
+    props: Props,
+}
+
+#[derive(Properties, Clone)]
+pub struct Props {
+    pub product: Product,
+    pub on_add_to_cart: Callback<()>,
+}
+
+impl Component for ProductCard {
+    type Message = ();
+    type Properties = Props;
+
+    fn create(props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        Self { props }
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        true
+    }
+
+    fn view(&self) -> Html {
+        let onclick = self.props.on_add_to_cart.reform(|_| ());
+
+        html! {
+          <div class="product_card_container">
+            <img class="product_card_image" src={&self.props.product.image} />
+            <div class="product_card_name">{&self.props.product.name}</div>
+            <div class="product_card_price">{"$"}{&self.props.product.price}</div>
+            <button class="product_atc_button" onclick=onclick>{"Add To Cart"}</button>
+          </div>
+        }
+    }
+}
---
+++ src/pages/home.rs
@@ -0,0 +1,152 @@
+use crate::api;
+use crate::components::ProductCard;
+use crate::types::{CartProduct, Product};
+use anyhow::Error;
+use yew::format::Json;
+use yew::services::fetch::FetchTask;
+use yew::prelude::*;
+
+struct State {
+    products: Vec<Product>,
+    cart_products: Vec<CartProduct>,
+    get_products_error: Option<Error>,
+    get_products_loaded: bool,
+}
+
+pub struct Home {
+    state: State,
+    link: ComponentLink<Self>,         // Allows us to register callback to trigger the update lifecycle method
+    task: Option<FetchTask>,
+}
+
+pub enum Msg {
+    AddToCart(i32),
+    GetProducts,
+    GetProductsSuccess(Vec<Product>),
+    GetProductsError(Error),
+}
+
+impl Component for Home {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_: Self::Properties, link:ComponentLink<Self>) -> Self {
+        let products = vec![];
+        let cart_products = vec![];
+
+        link.send_message(Msg::GetProducts);
+
+        Self {
+            state: State {
+                products,
+                cart_products,
+                get_products_error: None,
+                get_products_loaded: false,
+            },
+            link,
+            task: None,
+        }
+    }
+
+    // Update is the place where the logic to update the component, in this case `State`, or perform
+    // side-effects(like network requests) exist. It is invoked using a `Message` enum that contains
+    // all the action the component supports. When we return `true` from this method, the component
+    // is re-rendered.
+    fn update(&mut self, message: Self::Message) -> ShouldRender {
+        match message {
+            Msg::GetProducts => {
+                self.state.get_products_loaded = false;
+                let handler =
+                    self.link
+                        .callback(move |response: api::FetchResponse<Vec<Product>>| {
+                            let (_, Json(data)) = response.into_parts();
+                            match data {
+                                Ok(products) => Msg::GetProductsSuccess(products),
+                                Err(err) => Msg::GetProductsError(err),
+                            }
+                        });
+                self.task = Some(api::get_products(handler));
+                true
+            }
+            Msg::GetProductsSuccess(products) => {
+                self.state.products = products;
+                self.state.get_products_loaded = true;
+                true
+            }
+            Msg::GetProductsError(error) => {
+                self.state.get_products_error = Some(error);
+                self.state.get_products_loaded = true;
+                true
+            }
+            Msg::AddToCart(product_id) => {
+                let product = self
+                    .state
+                    .products
+                    .iter()
+                    .find(|p: &&Product| p.id == product_id)
+                    .unwrap();
+                let cart_product = self
+                    .state
+                    .cart_products
+                    .iter_mut()
+                    .find(|cp: &&mut CartProduct| cp.product.id == product_id);
+
+                if let Some(cp) = cart_product {
+                    cp.quantity += 1;
+                } else {
+                    self.state.cart_products.push(CartProduct {
+                        product: product.clone(),
+                        quantity: 1,
+                    })
+                }
+                true
+            }
+        }
+    }
+
+    fn change(&mut self, _: Self::Properties) -> ShouldRender {
+        true
+    }
+
+    fn view(&self) -> Html {
+        let products: Vec<Html> = self
+            .state
+            .products
+            .iter()
+            .map(|product: &Product| {
+                let product_id = product.id;
+                html! {
+                  <ProductCard product={product}on_add_to_cart=self.link.callback(move |_| Msg::AddToCart(product_id))/>
+                }
+            })
+            .collect();
+
+        let cart_value = self
+            .state
+            .cart_products
+            .iter()
+            .fold(0.0, |acc, cp| acc + (cp.quantity as f64 * cp.product.price));
+
+        if !self.state.get_products_loaded {
+            html! {
+               <div>{"Loading ..."}</div>
+            }
+        } else if let Some(_) = self.state.get_products_error {
+            html! {
+                <div>
+                  <span>{"Error loading products! :("}</span>
+                </div>
+            }
+        } else {
+            html! {
+                <div>
+                  <div class="navbar">
+                    <div class="navbar title">{"Starter"}</div>
+                    <div class="navbar_cart_value">{format!("Cart Value: {:.2}", cart_value)}</div>
+                  </div>
+                  <div class="product_card_list">{products}</div>
+                </div>
+            }
+        }
+    }
+}
